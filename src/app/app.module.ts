f6ff87e63f7767643af373fbc9e9ef8bcdd3bd2a
Gustavo Gomes Madureira <gustavo.gomes@tolentinos.com>
2021-03-29 23:10:03 -0300

    inicio tela home
    Começamos a estruturar a tela home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HeaderComponent } from './header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { HeaderTwoComponent } from './shared/header-two/header-two.component';
+import { HeaderOneComponent } from './shared/header-one/header-one.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderOneComponent,
+    HeaderTwoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
